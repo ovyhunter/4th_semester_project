b274f5e3ea366acc32edfcf569ddeeb0d59ebb48
ovyhunter <ovyhunter1996@gmail.com>
2019-05-31 22:09:50 +0200

    second page done

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ $(document).ready(function () {
     $('.animated-icon1').toggleClass('open');
   });
 });
+
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+$.scrollify({
+  section : "section",
+  sectionName : "panel",
+  interstitialSection : "",
+  easing: "easeOutExpo",
+  scrollSpeed: 1100,
+  offset : 0,
+  scrollbars: true,
+  standardScrollElements: "",
+  setHeights: true,
+  overflowScroll: true,
+  updateHash: true,
+  touchScroll:true,
+  before:function() {},
+  after:function() {},
+  afterResize:function() {},
+  afterRender:function() {}
+});
